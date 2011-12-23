5a0a1c85bfa85ce9b8c13d4767c546c76885a624
Neema Naficy <nnaficy@gmail.com>
2011-12-23 14:05:59 -0800

    Done with sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+# Schema version: 20100822233125
+#
+# Table name: users
+#
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#
+
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
@@ -19,11 +33,17 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
   
-  def self.authenticate(email, submitted_password)
-    user - find_by_email(email)
-    return nil  if user.nil?
-    return user if user.has_password?(submitted_password)
-  end    
+  class << self
+    def authenticate(email, submitted_password)
+      user = find_by_email(email)
+      (user && user.has_pssword?(submitted_password)) ? user : nil
+    end  
+  
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil
+    end    
+  end
   
   private
   
